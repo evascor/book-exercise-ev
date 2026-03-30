ef7bb1f41e0f1ecd2a4062a75ee23835cf1bf55d
evascor <elisabetvasco@gmail.com>
2026-03-30 21:49:56 +0200

    feat: add refNumber field, setter method setRefNumber and getter method getRefNumber

--- Book.java
+++ Book.java
@@ -12,6 +12,7 @@ class Book
     private String author;
     private String title;
     private int pages;
+    private String refNumber;
 
     /**
      * Set the author and title fields when this object
@@ -22,6 +23,7 @@ class Book
         author = bookAuthor;
         title = bookTitle;
         pages = bookPages;
+        refNumber = "";
     }
 
     // Return author's book
@@ -61,5 +63,17 @@ class Book
         System.out.println("Author: "+author);
         System.out.println("Pages: "+pages);
         System.out.println();
+    }
+
+    // Define reference number
+    public void setRefNumber(String ref)
+    {
+        refNumber = ref;
+    }
+
+    // Return reference number
+    public String getRefNumber()
+    {
+        return refNumber;
     }
 }
