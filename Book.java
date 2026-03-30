c5cb53dec5362894d68b1c279bf75a1d3b850b0e
evascor <elisabetvasco@gmail.com>
2026-03-30 21:21:38 +0200

    feat: add pages field and getPages method

--- Book.java
+++ Book.java
@@ -11,15 +11,17 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int bookPages)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = bookPages;
     }
 
     // Return author's book
@@ -32,6 +34,12 @@ class Book
     public String getTitle()
     {
         return title;
+    }
+
+    // Return number of pages
+    public int getPages()
+    {
+        return pages;
     }
 
     // Show Author on screen
